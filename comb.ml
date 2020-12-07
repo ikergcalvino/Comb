03d274794d2254b7e7a36b9786b8fd0c9a1ab1f1
Íker García Calviño <iker.gcalvino@udc.es>
2020-12-07 01:34:36 +0100

    fact upgrade

--- comb.ml
+++ comb.ml
@@ -1,6 +1,9 @@
-let rec fact n = 
-    if n > 0 then n * fact (n-1)
-    else 1;;
+let fact n = if n < 2
+    then 1
+    else let rec fact (i, r) = if i = n
+        then r
+        else fact (i + 1, (i + 1) * r)
+    in fact (2, 2);;
     
    
 let comb (m, n) = 

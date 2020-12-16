5bfe31da8a0fe822d69e2af14192de39cff767e1
Íker García Calviño <iker.gcalvino@udc.es>
2020-12-16 17:28:14 +0100

    format and cases 0 and m = n

--- comb.ml
+++ comb.ml
@@ -4,9 +4,7 @@ let fact n = if n < 2
         then r
         else fact (i + 1, (i + 1) * r)
     in fact (2, 2);;
-    
-   
+
 let comb (m, n) = 
-    fact m / fact (m-n) / fact n;;
-        
-    
+    if n = 0 || m = n then 1
+    else fact m / (fact n * fact (m-n));;
